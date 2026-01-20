5abcb28a89f065ff6bdc7238ac93023440c96938
Carl Edman <CarlEdman@gmail.com>
2026-01-19 21:08:33 -0500

    .

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+#! python3
+
+from settings import settings
+from templates import templates
+from static import static
--- src/server.py
+++ src/server.py
@@ -46,7 +46,7 @@ async def read_item(item_id: int):
 
 websockets : set[WebSocket] = set()
 
-async def send_to_all(msg: str):
+async def broadcast(msg: str):
   async with asyncio.TaskGroup() as tg:
     for w in websockets:
       tg.create_task(w.send_text(msg))
@@ -55,17 +55,15 @@ async def send_to_all(msg: str):
 async def websocket_endpoint(websocket: WebSocket):
   await websocket.accept()
   websockets.add(websocket)
-  await send_to_all(f"Socket {repr(websocket)} opened; Now {len(websockets)} WebSockets.")
+  await broadcast(f"Socket {repr(websocket)} opened; now {len(websockets)} WebSockets.")
   try:
     while True:
       data = await websocket.receive_text()
-      await send_to_all(f"Message text from {repr(websocket)} was: {data} {repr(settings)}")
+      await broadcast(f"Message text from {repr(websocket)} was: {data} {repr(settings)}")
   except WebSocketDisconnect:
-    # This block is executed when the client disconnects
     websockets.remove(websocket)
-    await send_to_all(f"Socket {repr(websocket)} disconnected; Now {len(websockets)} remaining WebSockets.")
+    await broadcast(f"Socket {repr(websocket)} disconnected; now {len(websockets)} remaining WebSockets.")
   except Exception as e:
-    # Handle other potential exceptions
     websockets.remove(websocket)
 
 if __name__ == "__main__":
